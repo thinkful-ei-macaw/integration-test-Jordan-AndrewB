5e1ace1bbeb316bc0282cf3f36eb51821f0d31f6
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-03-25 14:37:35 -0700

    final

--- app.js
+++ app.js
@@ -11,39 +11,40 @@ app.get('/apps', (req, res) => {
   const {sort, genres} = req.query;
   let googleArray = [...google];
 
-    if(sort) {
-        if(sort === 'Rating') {
-            googleArray.sort((a, b) => {
-                return a['Rating'] > b['Rating'] ? 1 : a['Rating'] < b['Rating'] ? -1 : 0;
-            })
-        } else if(sort === 'App') {
-            googleArray.sort((a,b) => {
-                return a['App'] > b['App'] ? 1 : a['App'] < b['App'] ? -1 : 0;
-            })
-        } else {
-            res
-                .status(400)
-                .send('You must select rating or app');
+  if(sort) {
+    if(sort === 'Rating') {
+      googleArray.sort((a, b) => {
+        return a['Rating'] > b['Rating'] ? 1 : a['Rating'] < b['Rating'] ? -1 : 0;
+      })
+    } else if(sort === 'App') {
+      googleArray.sort((a,b) => {
+        return a['App'] > b['App'] ? 1 : a['App'] < b['App'] ? -1 : 0;
+      })
+    } else {
+      res
+        .status(400)
+        .send('You must select rating or app');
 
-        }
-    } 
-
-    if(genres) {
-        const genre = [ 'Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
-
-        if(!genre.includes(genres)) {
-            res.status(400).send('Genre not included, please pick another');
-        } else {
-          
-            googleArray = googleArray.filter( item => {
-                return item['Genres'].includes(genres); 
-        })
-        }
     }
+  } 
+
+  if(genres) {
+    const genre = [ 'Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+
+    if(!genre.includes(genres)) {
+      res.status(400).send('Genre not included, please pick another');
+    } else {
+          
+      googleArray = googleArray.filter( item => {
+        return item['Genres'].includes(genres); 
+      });
+    }
+  }
   
   
   
-    res.json(googleArray);
+  
+  res.json(googleArray);
 });
   
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,6 @@
-const {expect} = require('chai');
+const {
+    expect
+} = require('chai');
 const supertest = require('supertest');
 const app = require('../app');
 
@@ -12,11 +14,65 @@ describe('app', () => {
                 .then(res => {
                     expect(res.body).to.be.an('array');
                     expect(res.body).to.have.lengthOf.at.least(1);
-                    expect(res.body[0]).to.include.keys
-                        'App', 'Rating'
-                    });
+                    expect(res.body[0]).to.include.keys 'App', 'Rating'
                 });
         });
     });
 
 
+    const validSortValues = ['Rating', 'App'];
+    validSortValues.forEach(validSort => {
+        it(`should return 200 with sorted array ${validSort}`, () => {
+            return supertest(app)
+                .get('./app')
+                .query({
+                    sort: validSort
+                })
+                .expect(200)
+                .then(res => {
+                    let i = 0,
+                        sorted = true;
+                    while (i < res.body.length - 1) {
+                        if (res.body[i][validSort]) {
+                            sorted = false;
+                            break;
+                        }
+                        i++;
+                    }
+                    expect(sorted).to.be.true;
+                });
+        });
+    })
+
+    it('return 400 if there is no rating or sort', () => {
+        return request(app)
+        .get('/apps')
+        .query( { sort: 'not valid'})
+        .expect(400, 'You need to select a sort!') 
+    })
+
+    it('should return 400 if genres parameter is invalid', () => {
+        return supertest(app)
+        .get('./apps')
+        .query({genres: 'invalid'})
+        .expect(400, 'Parameters genre must be valid')
+        
+    })
+
+
+    it('it should filter by genre', () => {
+        let expected = apps.filter(app => app["Genres"].includes("Action"));
+
+        return supertest(app)
+        .get('./apps')
+        .query( {genres: "Action"})
+        .expect(200)
+        .expect("Content-type", /json/)
+        .then(res => {
+            expect(res.body).to.be.an('arry');
+            expect(res.body).to.deep.equal(expected);
+        });
+    });
+
+
+})
